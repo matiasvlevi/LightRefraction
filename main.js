9dfc602e81ffa18b64a615be7e65ba965ff6fe40
Matias Vazquez-Levi <50473168+matiasvlevi@users.noreply.github.com>
2020-11-08 02:09:00 -0500

    made it better

--- main.js
+++ main.js
@@ -2,26 +2,27 @@
 let r1;
 let n1 = 1.00;
 let n2 = 1.45;
-let s1;
-let s2;
+
 let data = [];
+let wnx = 900;
+let wny = 800;
+let circleSize = 600;
 function setup() {
-  r1 = new Ray(100,100,30,400,true);
-  s1 = createSlider(0,3,n1,0.005);
-  s2 = createSlider(0,3,n2,0.005);
-  createCanvas(600,600);
-  data = printData(5);
+  r1 = new Ray(100,100,30,800,"Incident");
+
+  createCanvas(wnx,wny);
+  //data = printData(5);
 }
 
 function draw() {
   background(51);
+  dottedVLine(100,700)
+  n1 = document.getElementsByName("n1")[0].value;
+  n2 = document.getElementsByName("n2")[0].value;
 
-  n1 = s1.value();
-  n2 = s2.value();
-  if (mouseY < 300) {
-      r1.render(255);
-  }
-  line(0,300,600,300)
+  r1.render(255);
+  //line(wnx/2,0,wnx/2,wny);
+  line(0,wny/2,wnx,wny/2)
   drawMesures();
 }
 
@@ -31,61 +32,126 @@ class Ray {
     this.angle = a;
     this.length = length;
     this.refractedRay = undefined;
-    this.rIntensity = 0;
-    this.mIntensity = 0;
+    this.intensity = 255;
+    this.type = incident;
     this.mirroredRay = undefined;
-    this.incident = incident;
+    this.parentAngle = undefined;
     this.previous = [];
   }
-  render(intensity) {
+  render() {
     angleMode(DEGREES);
 
     if (this.previous.length > 0) {
-      let b = this.pos.x-300;
-      let a = this.pos.y-300;
+      let b = this.pos.x-(wnx/2);
+      let a = this.pos.y-(wny/2);
       let c = findLength(a,b)
-      this.angle = asin(b/c)//sin(mouseY)*findLength(300-mouseY,300-mouseX)//map(mouseX,0,600,-90,90);
+      this.angle = asin(b/c);
     }
 
     let x1 = this.pos.x;
     let y1 = this.pos.y;
     let x2 = (this.length*cos(this.angle+90))+x1;
     let y2 = (this.length*sin(this.angle+90))+y1;
+    if (y2 > wny/2 && this.type == "Incident") {
 
+        let p = lineIntersection(x1,y1,x2,y2,0,wny/2,wnx,wny/2);
+        x2 = p[0];
+        y2 = p[1];
+        this.mirroredRay = new Ray(0,0,0,0,"Mirrored");
+        this.mirroredRay.pos.x = x2;
+        this.mirroredRay.pos.y = y2;
+        this.mirroredRay.length = 600;
+        this.mirroredRay.angle = -1*((90+this.angle)-180)+90;
+        this.mirroredRay.parentAngle = this.angle;
+        let refAngle = findThetaR(n1,n2,this.angle);
+        this.refractedRay = new Ray(0,0,0,0,"Refracted");
+        this.refractedRay.pos.x = x2;
+        this.refractedRay.pos.y = y2;
+        this.refractedRay.length = 600;
+        this.refractedRay.angle = refAngle;
+        this.refractedRay.parentAngle = this.angle;
+        if (this.pos.y < wny/2) {
+            fill(255);
+            ellipse(x1,y1,6,6)
+        }
 
-    if (y2 > 300 && this.incident == true) {
-    fill(255);
-    ellipse(x1,y1,6,6)
-      let p = lineIntersection(x1,y1,x2,y2,0,300,600,300);
-      x2 = p[0];
-      y2 = p[1];
-      this.mirroredRay = new Ray(0,0,0,0,false);
-      this.mirroredRay.pos.x = x2;
-      this.mirroredRay.pos.y = y2;
-      this.mirroredRay.length = 600;
-      this.mirroredRay.angle = -1*((90+this.angle)-180)+90;
-      let refAngle = findThetaR(n1,n2,this.angle);
-      this.refractedRay = new Ray(0,0,0,0,false);
-      this.refractedRay.pos.x = x2;
-      this.refractedRay.pos.y = y2;
-      this.refractedRay.length = 600;
-      this.refractedRay.angle = refAngle;
-
-      this.rIntensity = calcIntensity_r(this.angle);
-      this.mIntensity = calcIntensity(this.angle);
     } else {
-      this.refractedRay = undefined;
+        this.refractedRay = undefined;
     }
-    if (this.refractedRay !== undefined && this.pos.y < 300) {
-      this.refractedRay.render(this.rIntensity);
-      this.mirroredRay.render(this.mIntensity);
-      //console.log(-1*(180 - (90 + this.angle)))
+    if (this.pos.y < wnx/2) {
+
+        if (this.refractedRay !== undefined ) {
+            this.refractedRay.render();
+        }
+        if (this.mirroredRay !== undefined) {
+            this.mirroredRay.render();
+        }
     }
 
+    if (this.type == "Refracted") {
+        //console.log(this.intensity)
+        this.intensity = calcIntensity_r(this.parentAngle);
+        noFill();
+        stroke(255)
+        //console.log(this.angle)
+        if (this.angle < 0) {
+            arc(this.pos.x,this.pos.y,64,64,(this.angle+90),90);
+        } else {
+            arc(this.pos.x,this.pos.y,64,64,90,(this.angle+90))
+        }
 
+    }
+    if (this.type == "Mirrored") {
+        this.intensity = calcIntensity(this.parentAngle);
+        noFill();
+        stroke(255)
+
+        if (this.angle < 180) {
+            arc(this.pos.x,this.pos.y,64,64,(this.angle+90),-90)
+        } else {
+            arc(this.pos.x,this.pos.y,64,64,-90,(this.angle+90))
+        }
+
+    }
+    if (this.type == "Incident" && this.pos.y > (wny/2)) {
+        stroke(this.intensity);
+
+    } else {
+
+        stroke(this.intensity);
+        line(x1,y1,x2,y2);
+    }
+    if (this.type !=="Incident") {
+        // let px = ((cos(this.angle+45) - sin(this.angle+45))*212)+(wnx/2);
+        // let py = ((sin(this.angle+45) + cos(this.angle+45))*212)+(wny/2);
+        let px2 = ((cos(this.angle+45) - sin(this.angle+45))*233)+(wnx/2);
+        let py2 = ((sin(this.angle+45) + cos(this.angle+45))*233)+(wny/2);
+        let ang = 0;
+        let ax = 0;
+        let ay = 0;
+        if (this.type == "Refracted") {
+            ang = -this.angle;
+            let anghalf = ang/2;
+
+            ax = ((cos(-anghalf+45) - sin(-anghalf+45))*45)+(wnx/2);
+            ay = ((sin(-anghalf+45) + cos(-anghalf+45))*45)+(wny/2);
+        } else {
+            ang = this.angle-180;
+            let anghalf = ang/2;
+
+            ax = ((cos(anghalf+180+45) - sin(anghalf+180+45))*45)+(wnx/2);
+            ay = ((sin(anghalf+180+45) + cos(anghalf+180+45))*45)+(wny/2);
+        }
+
+
+        noStroke();
+        fill(255)
+        textAlign(CENTER);
+        text(round(ang*10)/10,ax,ay);
+        //ellipse(px,py,3,3);
+    }
     this.previous = [x1,y1,x2,y2];
-    stroke(intensity);
-    line(x1,y1,x2,y2);
+
   }
 }
 function findThetaR(n1,n2,thetaI) {
@@ -109,37 +175,64 @@ function findLength(a,b) {
 function drawMesures() {
   noStroke()
   fill(255)
-  text("Indice de refraction: "+n1,10,20);
-  text("Indice de refraction: "+n2,10,580);
+  // text("Indice de refraction: "+n1,10,20);
+  // text("Indice de refraction: "+n2,10,580);
   noFill();
   stroke(255,100);
-  ellipse(300,300,400,400);
-
+  ellipse(wnx/2,wny/2,circleSize,circleSize);
+  for (let i = 0; i < 72; i++) {
+      let offset = 135+90;
+      let n = 5;
+    let x = (cos(i*n+offset)-sin(i*n+offset))*210+(wnx/2);
+    let y = (sin(i*n+offset)+cos(i*n+offset))*210+(wny/2);
+    push();
+    translate(x,y);
+    rotate(i*n);
+    stroke(100);
+    line(0,0,0,10);
+    pop();
+  }
+  for (let i = 0; i < 360; i++) {
+      let offset = 135+90;
+      let n = 1;
+    let x = (cos(i*n+offset)-sin(i*n+offset))*210+(wnx/2);
+    let y = (sin(i*n+offset)+cos(i*n+offset))*210+(wny/2);
+    push();
+    translate(x,y);
+    rotate(i*n);
+    stroke(100);
+    line(0,0,0,5);
+    pop();
+  }
   for (let i = 0;i <36; i++) {
     push();
     let offset = 135+90;
     let n = 10;
-    let x = (cos(i*n+offset)-sin(i*n+offset))*140+300;
-    let y = (sin(i*n+offset)+cos(i*n+offset))*140+300;
-    let xl = (cos(i*n+offset)-sin(i*n+offset))*150+300;
-    let yl = (sin(i*n+offset)+cos(i*n+offset))*150+300;
+    let x = (cos(i*n+offset)-sin(i*n+offset))*210+(wnx/2);
+    let y = (sin(i*n+offset)+cos(i*n+offset))*210+(wny/2);
+    let xl = (cos(i*n+offset)-sin(i*n+offset))*218+(wnx/2);
+    let yl = (sin(i*n+offset)+cos(i*n+offset))*218+(wny/2)+3;
 
     textSize(10);
     textAlign(CENTER)
     noStroke()
-    fill(255,170)
-    text(formatDeg(i*n),xl,yl);
+    fill(180)
+    let deg = formatDeg(i*n);
+    if (deg !== 90) {
+        text(deg,xl,yl);
+    }
+
     translate(x,y);
 
     rotate(i*n);
-    stroke(255,20);
-    line(0,0,0,150)
+    stroke(100);
+    line(0,0,0,20)
 
     pop()
   }
 
   fill(255,100)
-  ellipse(300,300,2,2);
+  ellipse(wnx/2,wny/2,2,2);
 }
 function formatDeg(x) {
   if (x >= 0 && x < 90) {
@@ -159,10 +252,14 @@ function calcIntensity_r(x) {
     return -2.26666*abs(x)+255;
 }
 function mouseDragged(){
-  if (mouseX > 0 && mouseX < 600 && mouseY < 600 && mouseY > 0) {
-    r1.pos.x = mouseX;
-    r1.pos.y = mouseY;
-  }
+
+    if (mouseX >= 0 && mouseX <= wnx && mouseY <= wny && mouseY >= 0) {
+        if (!(mouseX < 65 && mouseX > 0 && mouseY < 460 && mouseY > 350)) {
+            r1.pos.x = mouseX;
+            r1.pos.y = mouseY;
+        }
+        //console.log(mouseX,mouseY)
+    }
 
 }
 function printData(increment) {
@@ -175,9 +272,6 @@ function printData(increment) {
     }
     return arr;
 }
-
-
-
 function download_csv() {
     let inter = JSON.parse(prompt("Increment"));
     data = printData(inter);
@@ -193,4 +287,12 @@ function download_csv() {
     hiddenElement.target = '_blank';
     hiddenElement.download = 'refractionData.csv';
     hiddenElement.click();
+}
+function dottedVLine(y1,y2) {
+    push()
+    for (let i = y1; i < y2; i+=16) {
+        stroke(255,100);
+        line(wnx/2,i,wnx/2,i+4);
+    }
+    pop()
 }
